33f14cbe22954e89d120453157bc474cc7166bc7
haoxuexiaoyao <luoqianli2006@126.com>
2017-07-09 12:12:44 +0800

    微信机器人项目初始化
    1.spring boot项目初始化
    2.增加一些项目所需要的配置

---
+++ src/main/java/cn/com/banduo/wrobot/WrobotApplication.java
@@ -0,0 +1,12 @@
+package cn.com.banduo.wrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrobotApplication.class, args);
+	}
+}
